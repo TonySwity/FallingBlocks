5144dfa2072226e5693ef93759857d18cc4cbd98
TonySwity <goldocean@live.com>
2022-06-19 21:43:03 +0300

    add OnTriggerEnter@d and Chec Main camera != null

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,14 +9,19 @@ public class PlayerController : MonoBehaviour
     private void Start()
     {
         float halfPlayerWidth = transform.localScale.x / 2;
-        _screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+        
+        if (Camera.main)
+        {
+            _screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+        }
+        
     }
 
     private void Update()
     {
         float input = Input.GetAxisRaw("Horizontal");
         float velocity = input * _speed;
-        transform.Translate(Vector2.right * velocity * Time.deltaTime );
+        transform.Translate(Vector2.right * (velocity * Time.deltaTime) );
 
         if (transform.position.x < -_screenHalfWidthInWorldUnits)
         {
@@ -27,6 +31,14 @@ public class PlayerController : MonoBehaviour
         if (transform.position.x > _screenHalfWidthInWorldUnits)
         {
             transform.position = new Vector2(-_screenHalfWidthInWorldUnits, transform.position.y);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D triggerCollider)
+    {
+        if (triggerCollider.TryGetComponent(out FallingBlock fallingBlock))
+        {
+            Destroy(gameObject);
         }
     }
 }
