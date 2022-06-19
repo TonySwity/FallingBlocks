f95b7b0d1e1a69fe7c6815ab062e824f80e75d84
TonySwity <goldocean@live.com>
2022-06-19 18:22:13 +0300

    changed _screenHalfWidthInUnits

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,17 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float _speed = 7f;
 
-    private float screenHalfWidthInWorldUnits = 9.5f;
+    private float _screenHalfWidthInWorldUnits;
+
+    private void Start()
+    {
+        float halfPlayerWidth = transform.localScale.x / 2;
+        _screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+    }
 
     private void Update()
     {
@@ -12,14 +19,14 @@ public class PlayerController : MonoBehaviour
         float velocity = input * _speed;
         transform.Translate(Vector2.right * velocity * Time.deltaTime );
 
-        if (transform.position.x < -screenHalfWidthInWorldUnits)
+        if (transform.position.x < -_screenHalfWidthInWorldUnits)
         {
-            transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
+            transform.position = new Vector2(_screenHalfWidthInWorldUnits, transform.position.y);
         }
         
-        if (transform.position.x > screenHalfWidthInWorldUnits)
+        if (transform.position.x > _screenHalfWidthInWorldUnits)
         {
-            transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+            transform.position = new Vector2(-_screenHalfWidthInWorldUnits, transform.position.y);
         }
     }
 }
