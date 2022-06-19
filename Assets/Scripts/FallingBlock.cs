f7c0c02f27ec3f878b5ac8bd2f474fdc78a9cb92
TonySwity <goldocean@live.com>
2022-06-19 21:50:15 +0300

    add change speed over time

--- Assets/Scripts/FallingBlock.cs
+++ Assets/Scripts/FallingBlock.cs
@@ -1,8 +1,17 @@
+using System;
 using UnityEngine;
 
 public class FallingBlock : MonoBehaviour
 {
-    [SerializeField]private float _speed = 7f;
+    [SerializeField] private float _speedMin = 7f;
+    [SerializeField] private float _speedMax = 15f;
+    
+    private float _speed;
+
+    private void Start()
+    {
+        _speed = Mathf.Lerp(_speedMin, _speedMax, Difficulty.GetDifficultyPercent());
+    }
 
     private void Update()
     {
