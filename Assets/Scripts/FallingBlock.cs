841fd37d7ea7c2445732cde2cc9599d4489aaca2
TonySwity <goldocean@live.com>
2022-06-20 21:53:38 +0300

    add destroy game object

--- Assets/Scripts/FallingBlock.cs
+++ Assets/Scripts/FallingBlock.cs
@@ -4,16 +4,25 @@ public class FallingBlock : MonoBehaviour
 {
     [SerializeField] private float _speedMin = 7f;
     [SerializeField] private float _speedMax = 15f;
+
+    private float _visibleHeightThreshold;
     
     private float _speed;
 
     private void Start()
     {
         _speed = Mathf.Lerp(_speedMin, _speedMax, Difficulty.GetDifficultyPercent());
+
+        _visibleHeightThreshold = -Camera.main.orthographicSize - transform.localScale.y;
     }
 
     private void Update()
     {
         transform.Translate(Vector2.down * (_speed * Time.deltaTime));
+
+        if (transform.position.y < _visibleHeightThreshold)
+        {
+            Destroy(gameObject);
+        }
     }
 }
